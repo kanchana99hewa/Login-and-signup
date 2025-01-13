565979976aaef2a655a9c7bda05498ca40eec7f0
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2025-01-13 23:42:38 +0530

    nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
 import SignUp from "./Components/SignUp";
-
+import { Navbar } from "./Components/Navbar";
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar  />
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home/>} />
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,10 +1,26 @@
 
 
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+
+
+
 function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <AppBar sx={{ bgcolor: '#333' }}>
+    <Toolbar>
+        <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+            Tech Coffee Break
+        </Typography>
+       
+            
+          
+     
+    </Toolbar>
+</AppBar>
   )
 }
 
