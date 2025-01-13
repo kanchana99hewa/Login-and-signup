9b0912f514deb10647977894798d751ddeed5c9c
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2025-01-14 00:00:01 +0530

    grid

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -1,9 +1,28 @@
+import {useState} from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Grid, Link, Button, Paper, TextField, Typography } from "@mui/material";
 
 
 function SignUp() {
+    
   return (
     <div>
-      Signup
+          <Grid align="center" className="wrapper">
+                <Paper  sx={{width: {
+                    xs: '80vw',     // 0
+                    sm: '50vw',     // 600
+                    md: '40vw',     // 900
+                    lg: '30vw',     // 1200
+                    xl: '20vw',     // 1536 
+                },
+                height:{
+                    lg: '60vh',     // 1200px and up
+                }}}>
+                    <Typography component="h1" variant="h5" style={heading}> Signup </Typography>
+                   
+                </Paper>
+            </Grid>
     </div>
   )
 }
