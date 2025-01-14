21576924d62d6410698a04a0333196563753a58b
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2025-01-14 22:36:56 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -13,3 +13,8 @@ app.use(cors());
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Failed to connect to MongoDB', err));
+
+    
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
