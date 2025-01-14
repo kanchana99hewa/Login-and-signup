ad59d7020b4ac0fa47da31db334e3d2a81f89694
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2025-01-14 11:37:13 +0530

    sign up

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -1,15 +1,23 @@
 import {useState} from "react";
-import { useNavigate } from "react-router-dom";
+
 
 import { Grid, Link, Button, Paper, TextField, Typography } from "@mui/material";
 
 
 function SignUp() {
-    
-  return (
-    <div>
-          <Grid align="center" className="wrapper">
-                <Paper  sx={{width: {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+    const paperStyle = {padding: "2rem", margin: "100px auto", borderRadius:"1rem", boxShadow: "10px 10px 10px"};
+    const heading = {fontSize:"2.5rem", fontWeight:"600"}
+    const row = {display:"flex", marginTop:"2rem"}
+    const btnStyle={marginTop:"2rem", fontSize:"1.2rem", fontWeight:"700", backgroundColor:"blue", borderRadius:"0.5rem"};
+    return (
+        <div>
+            <Grid align="center" className="wrapper">
+                <Paper style={paperStyle} sx={{width: {
                     xs: '80vw',     // 0
                     sm: '50vw',     // 600
                     md: '40vw',     // 900
@@ -20,11 +28,17 @@ function SignUp() {
                     lg: '60vh',     // 1200px and up
                 }}}>
                     <Typography component="h1" variant="h5" style={heading}> Signup </Typography>
-                   
+                    <form onSubmit={handleSignup}>
+                        <TextField style={row} sx={{label: { fontWeight: '700', fontSize:"1.3rem" }}} fullWidth type="text" label="Enter Name" name="name" onChange={(e)=>setName(e.target.value)}></TextField>
+                        <TextField style={row} sx={{label: { fontWeight: '700', fontSize:"1.3rem" }}} fullWidth label="Email" variant="outlined" type="email" placeholder="Enter Email" name="email" onChange={(e)=>setEmail(e.target.value)}/>                    
+                        <TextField style={row} sx={{label: { fontWeight: '700', fontSize:"1.3rem" }}} fullWidth label="Password" variant="outlined" type="password" placeholder="Enter Password" name="password" onChange={(e)=>setPassword(e.target.value)} />
+                        <Button style={btnStyle} variant="contained" type="submit">SignUp</Button>
+                    </form>
+                    <p>Already have an account?<Link href="/login"> Login</Link></p>
                 </Paper>
             </Grid>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default SignUp
