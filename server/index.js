e17aff3fc7d0a5fbe1a58d4b7c4b6c1cdd03c5b5
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2025-01-14 22:19:38 +0530

    mongo express

--- server/index.js
+++ server/index.js
@@ -3,14 +3,11 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const dotenv = require("dotenv");
-const session = require("express-session");
-const MongoStore = require("connect-mongo");
-const UserModel = require("./model/User");
+
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: '', // Replace with your frontend's URL
-    credentials: true
-}));
+app.use(cors());
+
+mongoose.connect()
