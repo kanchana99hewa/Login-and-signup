ce56b4900cc21de7b7f009bbc18dcdad35451ed2
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2025-01-14 11:39:48 +0530

    paper

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -1,14 +1,10 @@
-import {useState} from "react";
 
 
 import { Grid, Link, Button, Paper, TextField, Typography } from "@mui/material";
 
 
 function SignUp() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+ 
 
     const paperStyle = {padding: "2rem", margin: "100px auto", borderRadius:"1rem", boxShadow: "10px 10px 10px"};
     const heading = {fontSize:"2.5rem", fontWeight:"600"}
@@ -28,10 +24,10 @@ function SignUp() {
                     lg: '60vh',     // 1200px and up
                 }}}>
                     <Typography component="h1" variant="h5" style={heading}> Signup </Typography>
-                    <form onSubmit={handleSignup}>
-                        <TextField style={row} sx={{label: { fontWeight: '700', fontSize:"1.3rem" }}} fullWidth type="text" label="Enter Name" name="name" onChange={(e)=>setName(e.target.value)}></TextField>
-                        <TextField style={row} sx={{label: { fontWeight: '700', fontSize:"1.3rem" }}} fullWidth label="Email" variant="outlined" type="email" placeholder="Enter Email" name="email" onChange={(e)=>setEmail(e.target.value)}/>                    
-                        <TextField style={row} sx={{label: { fontWeight: '700', fontSize:"1.3rem" }}} fullWidth label="Password" variant="outlined" type="password" placeholder="Enter Password" name="password" onChange={(e)=>setPassword(e.target.value)} />
+                    <form >
+                        <TextField style={row} sx={{label: { fontWeight: '700', fontSize:"1.3rem" }}} fullWidth type="text" label="Enter Name" name="name"></TextField>
+                        <TextField style={row} sx={{label: { fontWeight: '700', fontSize:"1.3rem" }}} fullWidth label="Email" variant="outlined" type="email" placeholder="Enter Email" name="email" />                    
+                        <TextField style={row} sx={{label: { fontWeight: '700', fontSize:"1.3rem" }}} fullWidth label="Password" variant="outlined" type="password" placeholder="Enter Password" name="password" />
                         <Button style={btnStyle} variant="contained" type="submit">SignUp</Button>
                     </form>
                     <p>Already have an account?<Link href="/login"> Login</Link></p>
