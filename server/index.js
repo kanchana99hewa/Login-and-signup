f724fb8286b703b326f9494a2ff9d8e48224ace6
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2025-01-14 12:55:06 +0530

    express

--- server/index.js
+++ server/index.js
@@ -6,3 +6,11 @@ const dotenv = require("dotenv");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const UserModel = require("./model/User");
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: '', // Replace with your frontend's URL
+    credentials: true
+}));
