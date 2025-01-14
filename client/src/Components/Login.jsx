d93210472b35f07cf6cbf4b2fad510b9e12fde55
Kanchana Hewamadduma <95676123+kanchana99hewa@users.noreply.github.com>
2025-01-14 12:09:25 +0530

    login

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,10 +1,33 @@
 
 
+import { Grid, Link, Button, Paper, TextField, Typography } from "@mui/material";
+
+
 function Login() {
   return (
     <div>
-       Login
-    </div>
+              <Grid align="center" className="wrapper">
+                <Paper style={paperStyle} sx={{width: {
+                    xs: '80vw',     // 0
+                    sm: '50vw',     // 600
+                    md: '40vw',     // 900
+                    lg: '30vw',     // 1200
+                    xl: '20vw',     // 1536 
+                },
+                height:{
+                    lg: '60vh',     // 1200px and up
+                }}}>
+                    <Typography component="h1" variant="h5" style={heading}> Signup </Typography>
+                    <form >
+                        <TextField style={row} sx={{label: { fontWeight: '700', fontSize:"1.3rem" }}} fullWidth type="text" label="Enter Name" name="name"></TextField>
+                        <TextField style={row} sx={{label: { fontWeight: '700', fontSize:"1.3rem" }}} fullWidth label="Email" variant="outlined" type="email" placeholder="Enter Email" name="email" />                    
+                        <TextField style={row} sx={{label: { fontWeight: '700', fontSize:"1.3rem" }}} fullWidth label="Password" variant="outlined" type="password" placeholder="Enter Password" name="password" />
+                        <Button style={btnStyle} variant="contained" type="submit">SignUp</Button>
+                    </form>
+                    <p>Already have an account?<Link href="/login"> Login</Link></p>
+                </Paper>
+            </Grid>
+</div>
   )
 }
 
